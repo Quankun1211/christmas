8fa25d30dfe8615273302f4ae684baeb36fdf76e
Quankun1211 <quan7ammm@gmail.com>
2023-11-30 09:58:41 +0700

    my noel

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+const yesBtn = document.getElementById('yes')
+const noBtn = document.getElementById('no')
+const select = document.querySelector('.select')
+const content = document.querySelector('.content')
+const giftBox = document.querySelector('.gift-box')
+const boxs = document.querySelectorAll('.box')
+const gift = document.querySelector('.gift-item')
+const giftItems = document.querySelectorAll('.gift-item')
+const close = document.querySelectorAll('.close')
+
+setTimeout(() =>{
+    yesBtn.onclick = () => {
+        select.classList.add('none')
+        giftBox.classList.remove('none')
+    }
+},4000)
+boxs.forEach((box, index) => {
+    const giftItem = giftItems[index]
+    box.onclick = () => {
+        giftItem.classList.add('active')
+        giftBox.classList.add('none')
+        console.log(index)
+    }
+})
+close.forEach((close, index) => {
+    const giftItem = giftItems[index]
+    close.onclick = () => {
+        giftItem.classList.remove('active')
+        giftBox.classList.remove('none')
+    }
+})
